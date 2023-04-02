56ebc1d8cbf7d4151493d6c70d409312473ac112
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-02 11:10:08 +0000

    laiks nr 5

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-# Anastasija Bondare 13.grupa 221RDB395
 # python3
 
-class Query: # Tiek definēta klase ar metodi _init_, 
-    def __init__(self, query): # kurā ir viens arguments "query".
-        self.type = query[0] # "type" vērtība tiek piešsaņemtaķirsaņemta kā pirmā vērtība sarakstā.
-        self.number = int(query[1]) # "number" vērtība tiek saņemta kā otrā skaitliskā (int) vērtība sarakstā.
-        if self.type == 'add': # Ja "type" ir vienāds ar "add",
-            self.name = query[2] # tad "name" tiek piešķirta trešā vērtība sarakstā
+class Query:
+    def __init__(self, query):
+        self.type = query[0]
+        self.number = int(query[1])
+        if self.type == 'add':
+            self.name = query[2]
 
-def read_queries(): # Tiek definēta lasīšanas funkcija
-    n = int(input()) # n - skaits, cik daudz vaicājumu (queries) tiks apstrādāti
-    return [Query(input().split()) for i in range(n)] # Tiek atgriezsts saraksts, kas tika izveidots no lietotāja ievadītajiem datiem
+def read_queries():
+    n = int(input())
+    return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
-    print('\n'.join(result)) # Ar motodi "join" tiek apvienoti visi elementi vienā rindā, kur katrs elements tiek atdalīts "\n" 
-    # ar jaunu rindu un pēc tam iegūtais rezultāts tiks izvadīts konsolē.
+    print('\n'.join(result))
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = [] # Tiek izveidots saraksts ar kontaktiem, kas satur visus telefona numurus
+    # Keep dictionary of contacts with numbers as keys and names as values.
+    contacts = {}
     for cur_query in queries:
         if cur_query.type == 'add':
-            contacts[cur_query.number] = cur_query.name # Pievieno jaunu ierakstu sarakstā
-            # if we already have contact with such number,
-            # we should rewrite contact's name
+            contacts[cur_query.number] = cur_query.name
         elif cur_query.type == 'del':
-            if cur_query.number in contacts:
-                contacts.pop(cur_query.number) # Tiek izdzēsts numurs no saraksta,ja tas iepriekš pastāvēja
+            contacts.pop(cur_query.number, None)
         else:
             response = contacts.get(cur_query.number, 'not found')
             result.append(response)
     return result
 
-if __name__ == '__main__': # Ja programma tiek izpildīta kā galvenā programma, tad
-    write_responses(process_queries(read_queries())) # tiek izsauktas funkcijas, kas izveidota sarakstu, apstrādā sarakstā iegūtos datus
-    # un pēc tam izvada rezultātu konsolē.
+if __name__ == '__main__':
+    write_responses(process_queries(read_queries()))
