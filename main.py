c8b91fd0dac6f95a8080c09e126d5e1cc4a4d0bf
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-02 21:11:52 +0000

    221RDB395 Anastasija Bondare 13.grupa

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 
 class Query: # Tiek definēta klase Query, kas satur metodi __init__,
     def __init__(self, query): # kura tiek izsaukta pēc objekta 'Query'.
-        self.type,self.number = query[0],int(query[1]) # Pirmā pozīcijā tiek saglabāts vaicājums jeb funkcija - add, del vai find.
-  # Otrajā pozīcijā tiek saglabāts telefona numurs kā vesels skaitlis.
+        self.type, self.number = query[0], int(query[1]) # Pirmā pozīcijā tiek saglabāts vaicājums jeb funkcija - add, del vai find.
+        # Otrajā pozīcijā tiek saglabāts telefona numurs kā vesels skaitlis.
         if self.type == 'add': # Ja vaicājums jeb funkcija ir vienāda ar add, tad 
             self.name = query[2] # Trešajā pozīcijā tiek saglabāts vārds, kas tika piešķirts telefona numuram.
 
-def read_queries(): # Tiek definēta "lasīšanas" funkcija. No tastatūras ievada vaicājamu skaitu, cik kontaktu tiks apstrādāti.
-    n = int(input()); return [Query(input().split()) for i in range(n)] # Tiek izveidots saraksts ar kontaktiem, kuri tiks aprstādāti pēc atbilstošām funkcijām.
+def read_queries(): # Tiek definēta "lasīšanas" funkcija. 
+    n = int(input()) # No tastatūras ievada vaicājamu skaitu, cik kontaktu tiks apstrādāti.
+    return [Query(input().split()) for i in range(n)] # Tiek izveidots saraksts ar kontaktiem, kuri tiks aprstādāti pēc atbilstošām funkcijām.
 
 def write_responses(result): print('\n'.join(result)) # Tiek izvadīti apstrādātie kontakti jaunajā rindā (katrs apstrādātais kontakts būs jaunajā rindā).
 
@@ -29,5 +30,7 @@ def process_queries(queries): # Tiek definēta "apstrādāšanas" funkcija.
                 result.append("not found") # Pretējā gadījumā tiks izvadīts paziņojums,ka kontakts netika atrasts.
     return result
 
+
 if __name__ == '__main__': # Izsauc funkcijas, lai apstrādātu ievadītos kontaktus un pēc tam tos izvadītu.
     write_responses(process_queries(read_queries()))
+
