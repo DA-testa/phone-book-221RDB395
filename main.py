9c0d3bf4a6840818d98dece06369c78d1d049a67
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-02 20:58:26 +0000

    221RDB395 Anastasija Bondare 13.grupa

--- main.py
+++ main.py
@@ -8,26 +8,25 @@ class Query: # Tiek definēta klase Query, kas satur metodi __init__,
         if self.type == 'add': # Ja vaicājums jeb funkcija ir vienāda ar add, tad 
             self.name = query[2] # Trešajā pozīcijā tiek saglabāts vārds, kas tika piešķirts telefona numuram.
 
-def read_queries(): # Tiek definēta "lasīšanas" funkcija. # No tastatūras ievada vaicājamu skaitu, cik kontaktu tiks apstrādāti.
-    n = int(input())
- return [Query(input().split()) for i in range(n)] # Tiek izveidots saraksts ar kontaktiem, kuri tiks aprstādāti pēc atbilstošām funkcijām.
+def read_queries(): # Tiek definēta "lasīšanas" funkcija. 
+    n = int(input()) # No tastatūras ievada vaicājamu skaitu, cik kontaktu tiks apstrādāti.
+    return [Query(input().split()) for i in range(n)] # Tiek izveidots saraksts ar kontaktiem, kuri tiks aprstādāti pēc atbilstošām funkcijām.
 
-def write_responses(result): print('\n'.join(result)) # Tiek izvadīti apstrādātie kontakti jaunajā rindā (katrs apstrādātais kontakts būs jaunajā rindā).
+def write_responses(result):
+    print('\n'.join(result)) # Tiek izvadīti apstrādātie kontakti jaunajā rindā (katrs apstrādātais kontakts būs jaunajā rindā).
 
 def process_queries(queries): # Tiek definēta "apstrādāšanas" funkcija.
     result = [] # Tiek definēts tukšs saraksts, kur glabās apstrādātos kontaktus (beigas).
     phone_book = {} # Tiek definēts tukšs saraksts ar kontaktiem, kurus lietotājs manuāli ievadīs no tastatūras (sākums).
     for current_query in queries: 
-        # Ja kontakts jau pastāv ar tādu pašu vārdu, tad tam tiks piešķirts jauns vārds.
-        if current_query.type == 'add': 
-            phone_book[current_query.number] = current_query.name 
+        if current_query.type == 'add':
+            phone_book[current_query.number] = current_query.name # Ja kontakts jau pastāv ar tādu pašu vārdu, tad tam tiks piešķirts jauns vārds.
         elif current_query.type == 'del':
             if current_query.number in phone_book:  # Ja kontaks jau pastāv sarakstā, 
                 del phone_book[current_query.number] # tad tas tiks izdzēsts no tā.
         else:
-            # Ja kontaksts jau pastāv esošā sarakstā, tad tiks izvadīts vārds piešķirtajam numuram.
-            if current_query.number in phone_book: 
-                result.append(phone_book[current_query.number])       
+            if current_query.number in phone_book: # Ja kontaksts jau pastāv esošā sarakstā,
+                result.append(phone_book[current_query.number]) # tad tiks izvadīts vārds piešķirtajam numuram.
             else:
                 result.append("not found") # Pretējā gadījumā tiks izvadīts paziņojums,ka kontakts netika atrasts.
     return result
