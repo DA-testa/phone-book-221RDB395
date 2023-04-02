bdada9e2a03d163bc58f334004da906225b916e2
Anastasija Bondare <125151183+221RDB395@users.noreply.github.com>
2023-04-02 21:25:24 +0300

    diena

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-# python
+# python3
+# 221RDB395 Anastasija Bondare 13.grupa
 
-def _hash_func(self, s):
-    ans = 0
-    for c in reversed(s):
-        ans = (ans * self._multiplier + ord(c)) % self._prime
-     return ans % self.bucket_count
-def add(self, string):
-    hashed = self._hash_func(string)
-    bucket = self.buckts[hashed]
-    if string not in bucket:
-        self.buckets[hashed] = [string] + bucket
-        
-def delete(self, string):
-    hashed = self._hash_func(string)
-    bucket = self.buckts[hashed]
-    for i in range(len(bucket)):
-        if bucket[i] == string:
-            bucket.pop(i)
-            break
+class Query:
+    def _init_(self, query):
+        self.type = query[0]
+        self.number = int(query[1])
+        if self.type == 'add':
+            self.name = query[2]
 
-def find(self, string):
-    hashed = self._hash_func(string)
-    if string in self.buckets[hashed]:
-        return "yes"
-    return "no"
+def read_queries():
+    n = int(input())
+    return [Query(input().split()) for i in range(n)]
+
+def write_responses(result):
+    print('\n'.join(result))
+
+def process_queries(queries):
+    result = []
+    contacts = []
+    for query in queries:
+        if query.type == 'add':
+            contacts[query.number] = query.name
+        elif query.type == 'del'
+            if query.number in contacts:
+                del contacts[query.number]
+        else:
+            response = contacts.get(query.number, 'not found')
+            result.append(response)
+    return result
+
+if name == 'main':
+    queries = read_queries()
+    results = process_queries(queries)
+    write_responses(results)
